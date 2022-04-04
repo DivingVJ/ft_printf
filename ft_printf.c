30e1b351cdc3bbeb5ec8973c99b69a75d45b863d
V vijay ananth Velayutham <vvelayut@u91z03s03.42kl.edu.my>
2022-04-04 19:14:33 +0800

    Variadic macros

--- ft_printf.c
+++ ft_printf.c
@@ -1,1 +1,51 @@
-/* Placeholder */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vvelayut <vvelayut@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/04 14:54:12 by vvelayut          #+#    #+#             */
+/*   Updated: 2022/04/04 19:10:10 by vvelayut         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h> 
+#include <stdarg.h>
+
+int	ft_printf(const char*, ...);
+
+int	main(void)
+{
+	printf("Testing Variadic Functions\n");
+	ft_printf("Hello");
+	return (0);
+}
+
+int	ft_printf(const char*, ...)
+{
+	int		i;
+	va_list	argptr;
+
+	i = 0;
+	va_start(argptr, number);
+	printf("number = %d \n", number);
+	while (i < number)
+	{
+		printf("Numbers %d \n", va_arg(argptr, int));
+		i = i + 1;
+	}
+	va_end(argptr);
+	return (0);
+}
+
+
+/* Cases to consider:
+Full text, string or character
+Number base 10
+Number Hex
+Pointer address
+Combination of both
+*/
+
