955473908d4a8460d4e9652b1c32e08ae1881fce
V Vijay Ananth <divingvj@gmail.com>
2022-04-29 00:44:03 +0800

    Create putptr to handle printing ptr address.

--- check_format.c
+++ check_format.c
@@ -15,18 +15,15 @@ int	check_format(const char *str, va_list argptr)
 	if (*str == 's')
 		len = ft_putstr_fd(va_arg(argptr, char *), 1);
 	if (*str == 'p')
-	{
-		len = ft_putstr_fd("0x", 1);
-		len = len + ft_putnbr_base_fd((unsigned long)va_arg(argptr, void *), base, 1);
-	}
+		len += ft_putnbr_b_fd((unsigned long)va_arg(argptr, void *), base, 1);
 	if ((*str == 'd') || (*str == 'i'))
 		len = ft_putnbr_fd(va_arg(argptr, int), 1);
 	if (*str == 'u')
 		len = ft_putunsignbr_fd(va_arg(argptr, unsigned int), 1);
 	if (*str == 'x')
-		len = ft_putnbr_base_fd(va_arg(argptr, unsigned int), base, 1);
+		len = ft_putnbr_b_fd(va_arg(argptr, unsigned int), base, 1);
 	if (*str == 'X')
-		len = ft_putnbr_base_fd(va_arg(argptr, unsigned int), cbase, 1);
+		len = ft_putnbr_b_fd(va_arg(argptr, unsigned int), cbase, 1);
 	if (*str == '%')
 		len = ft_putchar_fd('%', 1);
 	return (len);
